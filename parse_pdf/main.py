b445d43efa46d710acd9c4cbbc21fe28de879827
RatWithCoffee <mashaglotova2003@gmail.com>
2024-11-04 04:35:16 +0300

    change python file

--- parse_pdf/main.py
+++ parse_pdf/main.py
@@ -1,15 +1,31 @@
 import json
 import os
-
 import PyPDF2
 from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_text_splitters import SentenceTransformersTokenTextSplitter
+from langchain_experimental.text_splitter import SemanticChunker
+from langchain_huggingface import HuggingFaceEmbeddings
 
+
+# Путь к директории с PDF файлами
 directory_path = "InnoglobalHack_2024"
-output_file = "texts.json"
+# Путь к файлу для сохранения результатов
+output_file1 = "texts1.json"
+output_file2 = "texts2.json"
+output_file3 = "texts3.json"
 
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=100, chunk_overlap=0)
 
-with open(output_file, "w", encoding="utf-8") as out_file:
+#----------------------------------------------------------------------------------------
+all_texts2=[]
+print(1)
+text_embedder = HuggingFaceEmbeddings(model_name='intfloat/multilingual-e5-large')
+
+print(2)
+text_splitter = SemanticChunker(text_embedder, breakpoint_threshold_type="percentile", breakpoint_threshold_amount=95)
+print(3)
+print(text_splitter)
+
+with open(output_file3, "w", encoding="utf-8") as out_file:
     all_texts = []
     for filename in os.listdir(directory_path):
         if filename.endswith(".pdf"):
@@ -20,18 +36,22 @@ with open(output_file, "w", encoding="utf-8") as out_file:
 
             print(f"Processing {file_path}")
 
+            # Чтение текста из PDF
             with open(file_path, "rb") as file:
                 pdf_reader = PyPDF2.PdfReader(file)
                 document_text = ""
                 for page in pdf_reader.pages:
                     document_text += page.extract_text()
+            print("Docs cobran")
+            chunks = text_splitter.split_text(document_text)
+            print("chunks")
+            for t in chunks:
+                all_texts2.append({"text": t})
+            print(f"Всего чанков: {len(chunks)}")
+            print("Первые N чанков:")
+        break
+    res2 = {"chunks": all_texts2}
+    # Сохранение всех кусков текста в JSON файл
+    json.dump(res2, out_file, ensure_ascii=False, indent=4)
 
-            texts = text_splitter.split_text(document_text)
-            for t in texts:
-                all_texts.append({"text": t})
-
-    res = {"chunks": all_texts}
-
-    json.dump(res, out_file, ensure_ascii=False, indent=4)
-
-print(f"Тексты сохранены в файл {output_file}")
+    print(f"Тексты сохранены в файл {output_file3}")
